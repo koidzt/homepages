7c60a757a7d0ab486cfdd3fbad28b91f0cecdb82
koidzt <koi27792@gmail.com>
2023-04-25 22:47:35 +0700

    add sample homepage

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,12 +1,35 @@
-import BackToTop from '../../components/Scrolling/BackToTop';
-import { Paper } from '@mui/material';
+import AppBarWithBackToTop from '../../components/AppBar/AppBarWithBackToTop';
+import { Box, Grid, Paper, Stack, Typography } from '@mui/material';
+import MeImg from '../../asset/image/home.jpg';
 
-function index() {
+function Home() {
   return (
-    <BackToTop>
-      <Paper sx={{ p: 1 }}>Home</Paper>
-    </BackToTop>
+    <AppBarWithBackToTop sxAppBar={{ mb: [2, 0.75] }}>
+      <Paper>
+        <Grid container>
+          <Grid item xs={12} md={6}>
+            <Box component={'img'} src={MeImg} width={'100%'} />
+          </Grid>
+          <Grid item xs={12} md={6}>
+            <Stack height={'100%'} justifyContent={'center'} alignItems={'center'} spacing={3}>
+              <Box textAlign={'center'}>
+                <Typography variant="h4" color={'lightBlue.dark'}>
+                  Miss Thanawan Panitpongsri
+                </Typography>
+                <Typography variant="h4" color={'lightBlue.main'}>
+                  (Koi)
+                </Typography>
+              </Box>
+
+              <Typography variant="h4" color={'lightBlue.light'}>
+                Front-End Developer
+              </Typography>
+            </Stack>
+          </Grid>
+        </Grid>
+      </Paper>
+    </AppBarWithBackToTop>
   );
 }
 
-export default index;
+export default Home;
