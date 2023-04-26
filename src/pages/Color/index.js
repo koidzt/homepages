09777786b22bf28ab5fe317c80f76d6342033ea8
koidzt <koi27792@gmail.com>
2023-04-26 20:18:55 +0700

    sample button each  palette color

--- src/pages/Color/index.js
+++ src/pages/Color/index.js
@@ -1,320 +1,240 @@
+import { Fragment } from 'react';
 import AppBarWithBackToTop from '../../components/AppBar/AppBarWithBackToTop';
-import { Box, Grid, Paper, Typography } from '@mui/material';
+import { Box, Button, Grid, Paper, Stack, Typography } from '@mui/material';
+
+const TextColorTheme = (color) => (
+  <Box m={2}>
+    <Typography variant="h4" color={`${color}.light`}>
+      {color}.light
+    </Typography>
+    <Typography variant="h4" color={`${color}.main`}>
+      {color}.main
+    </Typography>
+    <Typography variant="h4" color={`${color}.dark`}>
+      {color}.dark
+    </Typography>
+  </Box>
+);
+
+const BgColorTheme = (color) => (
+  <Fragment>
+    <Box bgcolor={`${color}.light`} textAlign={'center'}>
+      <Typography variant="h5" lineHeight={'40px'} color={`${color}.contrastText`}>
+        {color}.light
+      </Typography>
+    </Box>
+    <Box bgcolor={`${color}.main`} textAlign={'center'}>
+      <Typography variant="h5" lineHeight={'40px'} color={`${color}.contrastText`}>
+        {color}.main
+      </Typography>
+    </Box>
+    <Box bgcolor={`${color}.dark`} textAlign={'center'}>
+      <Typography variant="h5" lineHeight={'40px'} color={`${color}.contrastText`}>
+        {color}.dark
+      </Typography>
+    </Box>
+  </Fragment>
+);
+
+const ButtonColorTheme = (color) => (
+  <Box p={1} border={'2px dashed'} borderColor={`${color}.main`} borderRadius={'12px'}>
+    <Stack direction={'row'} justifyContent={'space-around'} alignItems={'center'}>
+      <Box borderRadius={'50%'} bgcolor={`${color}.main`} width={'20px'} height={'20px'} />
+      <Typography variant="h5" lineHeight={'40px'} color={`${color}.main`}>
+        {color}
+      </Typography>
+      <Box borderRadius={'50%'} bgcolor={`${color}.main`} width={'20px'} height={'20px'} />
+    </Stack>
+
+    <Stack spacing={1} alignItems={'center'}>
+      <Button variant="text" color={`${color}`} sx={{ width: '250px' }}>
+        <Typography variant="button" lineHeight={'40px'}>
+          text
+        </Typography>
+      </Button>
+
+      <Button variant="outlined" color={`${color}`} sx={{ width: '250px' }}>
+        <Typography variant="button" lineHeight={'40px'}>
+          outlined
+        </Typography>
+      </Button>
+
+      <Button variant="contained" color={`${color}`} sx={{ width: '250px' }}>
+        <Typography variant="button" lineHeight={'40px'}>
+          contained
+        </Typography>
+      </Button>
+    </Stack>
+  </Box>
+);
 
 function Color() {
   return (
     <AppBarWithBackToTop sxAppBar={{ mb: [2, 3] }}>
-      <Paper sx={{ m: 2 }}>
-        <Typography variant="h2" textAlign={'center'}>
-          Color Theme
-        </Typography>
+      <Typography variant="h2" textAlign={'center'}>
+        Color Theme
+      </Typography>
+
+      {/* Text */}
+      <Paper sx={{ mb: [2, 3] }}>
         <Grid container>
-          <Grid item xs={12} md={3} textAlign={'center'}>
-            <Box m={2}>
-              <Typography variant="h4" color={'primary.light'}>
-                primary.light
-              </Typography>
-              <Typography variant="h4" color={'primary.main'}>
-                primary.main
-              </Typography>
-              <Typography variant="h4" color={'primary.dark'}>
-                primary.dark
-              </Typography>
-            </Box>
-
-            <Box m={2}>
-              <Typography variant="h4" color={'secondary.light'}>
-                secondary.light
-              </Typography>
-              <Typography variant="h4" color={'secondary.main'}>
-                secondary.main
-              </Typography>
-              <Typography variant="h4" color={'secondary.dark'}>
-                secondary.dark
-              </Typography>
-            </Box>
-
-            <Box m={2}>
-              <Typography variant="h4" color={'error.light'}>
-                error.light
-              </Typography>
-              <Typography variant="h4" color={'error.main'}>
-                error.main
-              </Typography>
-              <Typography variant="h4" color={'error.dark'}>
-                error.dark
-              </Typography>
-            </Box>
-
-            <Box m={2}>
-              <Typography variant="h4" color={'warning.light'}>
-                warning.light
-              </Typography>
-              <Typography variant="h4" color={'warning.main'}>
-                warning.main
-              </Typography>
-              <Typography variant="h4" color={'warning.dark'}>
-                warning.dark
-              </Typography>
-            </Box>
-
-            <Box m={2}>
-              <Typography variant="h4" color={'info.light'}>
-                info.light
-              </Typography>
-              <Typography variant="h4" color={'info.main'}>
-                info.main
-              </Typography>
-              <Typography variant="h4" color={'info.dark'}>
-                info.dark
-              </Typography>
-            </Box>
-
-            <Box m={2}>
-              <Typography variant="h4" color={'success.light'}>
-                success.light
-              </Typography>
-              <Typography variant="h4" color={'success.main'}>
-                success.main
-              </Typography>
-              <Typography variant="h4" color={'success.dark'}>
-                success.dark
-              </Typography>
-            </Box>
-
-            <Box m={2}>
-              <Typography variant="h4" color={'grey.light'}>
-                grey.light
-              </Typography>
-              <Typography variant="h4" color={'grey.main'}>
-                grey.main
-              </Typography>
-              <Typography variant="h4" color={'grey.dark'}>
-                grey.dark
-              </Typography>
-            </Box>
+          <Grid item xs={12} textAlign={'center'}>
+            <Typography variant="h4" textAlign={'center'} mt={2}>
+              Text Color
+            </Typography>
           </Grid>
 
+          {/* Col1 */}
           <Grid item xs={12} md={3} textAlign={'center'}>
-            <Box m={2}>
-              <Typography variant="h4" color={'red.light'}>
-                red.light
-              </Typography>
-              <Typography variant="h4" color={'red.main'}>
-                red.main
-              </Typography>
-              <Typography variant="h4" color={'red.dark'}>
-                red.dark
-              </Typography>
-            </Box>
-
-            <Box m={2}>
-              <Typography variant="h4" color={'pink.light'}>
-                pink.light
-              </Typography>
-              <Typography variant="h4" color={'pink.main'}>
-                pink.main
-              </Typography>
-              <Typography variant="h4" color={'pink.dark'}>
-                pink.dark
-              </Typography>
-            </Box>
-
-            <Box m={2}>
-              <Typography variant="h4" color={'purple.light'}>
-                purple.light
-              </Typography>
-              <Typography variant="h4" color={'purple.main'}>
-                purple.main
-              </Typography>
-              <Typography variant="h4" color={'purple.dark'}>
-                purple.dark
-              </Typography>
-            </Box>
-
-            <Box m={2}>
-              <Typography variant="h4" color={'deepPurple.light'}>
-                deepPurple.light
-              </Typography>
-              <Typography variant="h4" color={'deepPurple.main'}>
-                deepPurple.main
-              </Typography>
-              <Typography variant="h4" color={'deepPurple.dark'}>
-                deepPurple.dark
-              </Typography>
-            </Box>
-
-            <Box m={2}>
-              <Typography variant="h4" color={'indigo.light'}>
-                indigo.light
-              </Typography>
-              <Typography variant="h4" color={'indigo.main'}>
-                indigo.main
-              </Typography>
-              <Typography variant="h4" color={'indigo.dark'}>
-                indigo.dark
-              </Typography>
-            </Box>
-
-            <Box m={2}>
-              <Typography variant="h4" color={'blue.light'}>
-                blue.light
-              </Typography>
-              <Typography variant="h4" color={'blue.main'}>
-                blue.main
-              </Typography>
-              <Typography variant="h4" color={'blue.dark'}>
-                blue.dark
-              </Typography>
-            </Box>
+            {TextColorTheme('primary')}
+            {TextColorTheme('secondary')}
+            {TextColorTheme('error')}
+            {TextColorTheme('warning')}
+            {TextColorTheme('info')}
+            {TextColorTheme('success')}
           </Grid>
 
+          {/* Col2 */}
           <Grid item xs={12} md={3} textAlign={'center'}>
-            <Box m={2}>
-              <Typography variant="h4" color={'lightBlue.light'}>
-                lightBlue.light
-              </Typography>
-              <Typography variant="h4" color={'lightBlue.main'}>
-                lightBlue.main
-              </Typography>
-              <Typography variant="h4" color={'lightBlue.dark'}>
-                lightBlue.dark
-              </Typography>
-            </Box>
-
-            <Box m={2}>
-              <Typography variant="h4" color={'cyan.light'}>
-                cyan.light
-              </Typography>
-              <Typography variant="h4" color={'cyan.main'}>
-                cyan.main
-              </Typography>
-              <Typography variant="h4" color={'cyan.dark'}>
-                cyan.dark
-              </Typography>
-            </Box>
-
-            <Box m={2}>
-              <Typography variant="h4" color={'teal.light'}>
-                teal.light
-              </Typography>
-              <Typography variant="h4" color={'teal.main'}>
-                teal.main
-              </Typography>
-              <Typography variant="h4" color={'teal.dark'}>
-                teal.dark
-              </Typography>
-            </Box>
-
-            <Box m={2}>
-              <Typography variant="h4" color={'green.light'}>
-                green.light
-              </Typography>
-              <Typography variant="h4" color={'green.main'}>
-                green.main
-              </Typography>
-              <Typography variant="h4" color={'green.dark'}>
-                green.dark
-              </Typography>
-            </Box>
-
-            <Box m={2}>
-              <Typography variant="h4" color={'lightGreen.light'}>
-                lightGreen.light
-              </Typography>
-              <Typography variant="h4" color={'lightGreen.main'}>
-                lightGreen.main
-              </Typography>
-              <Typography variant="h4" color={'lightGreen.dark'}>
-                lightGreen.dark
-              </Typography>
-            </Box>
-
-            <Box m={2}>
-              <Typography variant="h4" color={'lime.light'}>
-                lime.light
-              </Typography>
-              <Typography variant="h4" color={'lime.main'}>
-                lime.main
-              </Typography>
-              <Typography variant="h4" color={'lime.dark'}>
-                lime.dark
-              </Typography>
-            </Box>
+            {TextColorTheme('red')}
+            {TextColorTheme('pink')}
+            {TextColorTheme('purple')}
+            {TextColorTheme('deepPurple')}
+            {TextColorTheme('indigo')}
+            {TextColorTheme('blue')}
           </Grid>
 
+          {/* Col3 */}
           <Grid item xs={12} md={3} textAlign={'center'}>
-            <Box m={2}>
-              <Typography variant="h4" color={'yellow.light'}>
-                yellow.light
-              </Typography>
-              <Typography variant="h4" color={'yellow.main'}>
-                yellow.main
-              </Typography>
-              <Typography variant="h4" color={'yellow.dark'}>
-                yellow.dark
-              </Typography>
-            </Box>
+            {TextColorTheme('lightBlue')}
+            {TextColorTheme('cyan')}
+            {TextColorTheme('teal')}
+            {TextColorTheme('green')}
+            {TextColorTheme('lightGreen')}
+            {TextColorTheme('lime')}
+          </Grid>
 
-            <Box m={2}>
-              <Typography variant="h4" color={'amber.light'}>
-                amber.light
-              </Typography>
-              <Typography variant="h4" color={'amber.main'}>
-                amber.main
-              </Typography>
-              <Typography variant="h4" color={'amber.dark'}>
-                amber.dark
-              </Typography>
-            </Box>
+          {/* Col4 */}
+          <Grid item xs={12} md={3} textAlign={'center'}>
+            {TextColorTheme('yellow')}
+            {TextColorTheme('amber')}
+            {TextColorTheme('orange')}
+            {TextColorTheme('deepOrange')}
+            {TextColorTheme('brown')}
+            {TextColorTheme('grey')}
+            {TextColorTheme('blueGrey')}
+          </Grid>
+        </Grid>
+      </Paper>
 
-            <Box m={2}>
-              <Typography variant="h4" color={'orange.light'}>
-                orange.light
-              </Typography>
-              <Typography variant="h4" color={'orange.main'}>
-                orange.main
-              </Typography>
-              <Typography variant="h4" color={'orange.dark'}>
-                orange.dark
-              </Typography>
-            </Box>
+      {/* Bg */}
+      <Paper sx={{ mb: [2, 3] }}>
+        <Grid container>
+          <Grid item xs={12} textAlign={'center'}>
+            <Typography variant="h4" textAlign={'center'} mt={2}>
+              Bg Color
+            </Typography>
+          </Grid>
 
-            <Box m={2}>
-              <Typography variant="h4" color={'deepOrange.light'}>
-                deepOrange.light
-              </Typography>
-              <Typography variant="h4" color={'deepOrange.main'}>
-                deepOrange.main
-              </Typography>
-              <Typography variant="h4" color={'deepOrange.dark'}>
-                deepOrange.dark
-              </Typography>
-            </Box>
+          {/* Col1 */}
+          <Grid item xs={12} md={3}>
+            {BgColorTheme('primary')}
+            {BgColorTheme('secondary')}
+            {BgColorTheme('error')}
+            {BgColorTheme('warning')}
+            {BgColorTheme('info')}
+            {BgColorTheme('success')}
+          </Grid>
 
-            <Box m={2}>
-              <Typography variant="h4" color={'brown.light'}>
-                brown.light
-              </Typography>
-              <Typography variant="h4" color={'brown.main'}>
-                brown.main
-              </Typography>
-              <Typography variant="h4" color={'brown.dark'}>
-                brown.dark
-              </Typography>
-            </Box>
+          {/* Col2 */}
+          <Grid item xs={12} md={3}>
+            {BgColorTheme('red')}
+            {BgColorTheme('pink')}
+            {BgColorTheme('purple')}
+            {BgColorTheme('deepPurple')}
+            {BgColorTheme('indigo')}
+            {BgColorTheme('blue')}
+          </Grid>
 
-            <Box m={2}>
-              <Typography variant="h4" color={'blueGrey.light'}>
-                blueGrey.light
-              </Typography>
-              <Typography variant="h4" color={'blueGrey.main'}>
-                blueGrey.main
-              </Typography>
-              <Typography variant="h4" color={'blueGrey.dark'}>
-                blueGrey.dark
-              </Typography>
-            </Box>
+          {/* Col3 */}
+          <Grid item xs={12} md={3}>
+            {BgColorTheme('lightBlue')}
+            {BgColorTheme('cyan')}
+            {BgColorTheme('teal')}
+            {BgColorTheme('green')}
+            {BgColorTheme('lightGreen')}
+            {BgColorTheme('lime')}
+          </Grid>
+
+          {/* Col4 */}
+          <Grid item xs={12} md={3}>
+            {BgColorTheme('yellow')}
+            {BgColorTheme('amber')}
+            {BgColorTheme('orange')}
+            {BgColorTheme('deepOrange')}
+            {BgColorTheme('brown')}
+            {BgColorTheme('grey')}
+            {BgColorTheme('blueGrey')}
+          </Grid>
+        </Grid>
+      </Paper>
+
+      {/* Button */}
+      <Paper sx={{ mb: [2, 3] }}>
+        <Grid container spacing={1} p={1}>
+          <Grid item xs={12} textAlign={'center'}>
+            <Typography variant="h4" textAlign={'center'} mt={2}>
+              Button Color
+            </Typography>
+          </Grid>
+
+          {/* Col1 */}
+          <Grid item xs={12} md={3}>
+            <Stack spacing={1}>
+              {ButtonColorTheme('primary')}
+              {ButtonColorTheme('secondary')}
+              {ButtonColorTheme('error')}
+              {ButtonColorTheme('warning')}
+              {ButtonColorTheme('info')}
+              {ButtonColorTheme('success')}
+            </Stack>
+          </Grid>
+
+          {/* Col2 */}
+          <Grid item xs={12} md={3}>
+            <Stack spacing={1}>
+              {ButtonColorTheme('red')}
+              {ButtonColorTheme('pink')}
+              {ButtonColorTheme('purple')}
+              {ButtonColorTheme('deepPurple')}
+              {ButtonColorTheme('indigo')}
+              {ButtonColorTheme('blue')}
+            </Stack>
+          </Grid>
+
+          {/* Col3 */}
+          <Grid item xs={12} md={3}>
+            <Stack spacing={1}>
+              {ButtonColorTheme('lightBlue')}
+              {ButtonColorTheme('cyan')}
+              {ButtonColorTheme('teal')}
+              {ButtonColorTheme('green')}
+              {ButtonColorTheme('lightGreen')}
+              {ButtonColorTheme('lime')}
+            </Stack>
+          </Grid>
+
+          {/* Col4 */}
+          <Grid item xs={12} md={3}>
+            <Stack spacing={1}>
+              {ButtonColorTheme('yellow')}
+              {ButtonColorTheme('amber')}
+              {ButtonColorTheme('orange')}
+              {ButtonColorTheme('deepOrange')}
+              {ButtonColorTheme('brown')}
+              {ButtonColorTheme('grey')}
+              {ButtonColorTheme('blueGrey')}
+            </Stack>
           </Grid>
         </Grid>
       </Paper>
