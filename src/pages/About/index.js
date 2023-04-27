bf2e2281e098280b3a55be0f4fdf744c8ae96e1f
koidzt <koi27792@gmail.com>
2023-04-27 22:09:18 +0700

    change image of about page and set responsive square image

--- src/pages/About/index.js
+++ src/pages/About/index.js
@@ -1,5 +1,5 @@
 import AppBarWithBackToTop from '../../components/AppBar/AppBarWithBackToTop';
-import { Box, Grid, Link, Paper, Stack, Typography } from '@mui/material';
+import { Avatar, Box, Grid, Link, Paper, Stack, Typography } from '@mui/material';
 import { RowData } from '../../components/DataBox';
 import { about } from '../../store/about';
 import MyselfImg from '../../asset/image/about.jpg';
@@ -8,26 +8,32 @@ function About() {
   return (
     <AppBarWithBackToTop>
       <Paper sx={{ p: 3, mb: [2, 3] }}>
-        <Grid container justifyContent={'center'} mb={5} sx={{ display: { md: 'none' } }}>
-          <Grid item xs={12} md={3}>
-            <Box
-              component={'img'}
-              alt={'my photo'}
-              src={MyselfImg}
-              width={'100%'}
-              borderRadius={'50%'}
-              border={'7px outset'}
-              borderColor={'amber.main'}
-            />
+        <Grid container justifyContent={'center'} mb={5} sx={{ display: { sm: 'none' } }}>
+          <Grid item xs={12} sm={3}>
+            <Box position={'relative'} pb={'100%'}>
+              <Box
+                component={'img'}
+                id={'imageMe'}
+                alt={'my photo'}
+                src={MyselfImg}
+                width={'100%'}
+                height={'100%'}
+                borderRadius={'50%'}
+                border={'7px outset'}
+                borderColor={'info.light'}
+                sx={{ objectFit: 'scale-down', objectPosition: '50% 50%' }}
+                position={'absolute'}
+              />
+            </Box>
           </Grid>
         </Grid>
 
         <Grid container position={'relative'} mb={10}>
-          <Grid item xs={12} md={9}>
+          <Grid item xs={12} sm={9}>
             <RowData {...about.info} />
           </Grid>
 
-          <Grid item xs={12} md={3}>
+          <Grid item xs={12} sm={3}>
             <Box
               component={'img'}
               alt={'my photo'}
@@ -36,11 +42,11 @@ function About() {
               height={'250px'}
               borderRadius={'50%'}
               border={'7px outset'}
-              borderColor={'amber.main'}
+              borderColor={'info.light'}
               position={'absolute'}
               top={0}
               right={0}
-              sx={{ display: { xs: 'none', md: 'block' } }}
+              sx={{ objectFit: 'scale-down', display: { xs: 'none', sm: 'block' } }}
             />
           </Grid>
         </Grid>
@@ -66,9 +72,9 @@ function About() {
         </Grid>
 
         <Grid container alignItems={'flex-end'}>
-          <Grid item xs={12} md={4} mb={[10, 0]}>
+          <Grid item xs={12} sm={6} md={4} mb={[10, 0, 0]}>
             <Box>
-              <Typography variant="h5" color={'primary.dark'} textAlign={{ xs: 'center', md: 'start' }}>
+              <Typography variant="h5" color={'primary.dark'} textAlign={{ xs: 'center', sm: 'start' }}>
                 {about.link.title.toLocaleUpperCase()}
               </Typography>
 
@@ -77,7 +83,7 @@ function About() {
                   key={`skill-${list.subtitle}`}
                   direction={'row'}
                   alignItems={'center'}
-                  justifyContent={{ xs: 'center', md: 'start' }}
+                  justifyContent={{ xs: 'center', sm: 'start' }}
                 >
                   <Typography variant="subtitle1" mr={1} color={'primary'}>
                     {list.subtitle} :
@@ -100,7 +106,7 @@ function About() {
             </Box>
           </Grid>
 
-          <Grid item xs={12} md={8}>
+          <Grid item xs={12} sm={6} md={8}>
             <RowData {...about.contact} textAlign="endReverse" />
           </Grid>
         </Grid>
